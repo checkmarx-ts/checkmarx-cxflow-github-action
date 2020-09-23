d9f781a6c89c2d8c43f7def7e38c6bdaee6f99e3
Ken McDonald <ken.mcdonald@checkmarx.com>
2020-09-23 16:20:58 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar /app/cx-flow.jar --spring.config.location=/app/application.yml --scan --cx-team="${TEAM}" --cx-project="${PROJECT}" --app="${APP}" --f=. ${CXFLOW_PARAMS}
+java -jar /app/cx-flow.jar --spring.config.location=/app/application.yml --scan --cx-team="${TEAM}" --cx-project="${PROJECT}" --github.api-url="${GITHUB_API_URL}/repos/" --app="${APP}" --f=. ${CXFLOW_PARAMS}
