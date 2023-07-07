f9dc0dcca6035af7f0ad6bc872eda8513896a3ec
satyamchaurasiapersistent <102941840+satyamchaurasiapersistent@users.noreply.github.com>
2023-07-07 17:57:15 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,22 @@
 if [ -n "$EXTRA_CERTIFICATES" ]
 then
         echo "Importing custom certificates in $EXTRA_CERTIFICATES subdirectory to '$JVMKEYTOOL_PATH'"
-        abc.sh /github/workspace/$EXTRA_CERTIFICATES "$JVMKEYTOOL_PATH"
+
+certs_dir=/github/workspace/$EXTRA_CERTIFICATES
+jvm_keystore=$JVMKEYTOOL_PATH
+count=1
+for cert in $certs_dir/*.crt
+do
+  echo "importing cert $cert"
+  keytool -trustcacerts -importcert \
+    -alias "checkmarx${count}" \
+    -file "$cert" \
+    -keystore "$jvm_keystore" \
+    -storepass 'changeit' -noprompt
+
+  # increment count
+  ((count++))
+done
 fi
 
 java ${JAVA_OPTS} -jar /app/cx-flow.jar --SHA=$GITHUB_SHA --spring.profiles.active="${CX_FLOW_ENABLED_VULNERABILITY_SCANNERS}" --scan --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --app="${APP}" --jira.url="${JIRA_URL}" --jira.username="${JIRA_USERNAME}" --jira.token="${JIRA_TOKEN}" --jira.project="${JIRA_PROJECT}" --jira.issue-type="${JIRA_ISSUE_TYPE}" --jira.open-transition="${JIRA_OPEN_TRANSITION}" --jira.close-transition="${JIRA_CLOSE_TRANSITION}" --jira.open-status="${JIRA_OPEN_STATUS}" --jira.closed-status="${JIRA_CLOSED_STATUS}" --f=. ${CXFLOW_PARAMS} 2> scanid$GITHUB_SHA.txt
