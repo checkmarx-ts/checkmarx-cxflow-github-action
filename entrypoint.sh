89aa8fc392361a697db1486520a27f14b1b81d78
satyamchaurasiapersistent <102941840+satyamchaurasiapersistent@users.noreply.github.com>
2023-08-04 11:53:20 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,11 @@
 #!/bin/sh
-docker ps -a
-ls -la
+echo ls -lrt
 # import custom certificates into java cacerts keystore
 if [ -n "$EXTRA_CERTIFICATES" ]
 then
-        echo "Importing custom certificates in $EXTRA_CERTIFICATES subdirectory to '$JVMKEYTOOL_PATH'"
-
-certs_dir=/github/workspace/$EXTRA_CERTIFICATES
-jvm_keystore=$JVMKEYTOOL_PATH
-count=1
-for cert in $certs_dir/*.crt
-do
-  echo "importing cert $cert"
-  keytool -trustcacerts -importcert \
-    -alias "checkmarx${count}" \
-    -file "$cert" \
-    -keystore "$jvm_keystore" \
-    -storepass 'changeit' -noprompt
-
-  # increment count
-  ((count++))
-done
+        jvm_keystore="/etc/ssl/certs/java/cacerts" # java 8 specific location
+        echo "Importing custom certificates in $EXTRA_CERTIFICATES subdirectory to '$jvm_keystore'"
+        /app/keytool-import-certs.sh /github/workspace/$EXTRA_CERTIFICATES "$jvm_keystore"
 fi
 
 java ${JAVA_OPTS} -jar /app/cx-flow.jar --SHA=$GITHUB_SHA --spring.profiles.active="${CX_FLOW_ENABLED_VULNERABILITY_SCANNERS}" --scan --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --app="${APP}" --jira.url="${JIRA_URL}" --jira.username="${JIRA_USERNAME}" --jira.token="${JIRA_TOKEN}" --jira.project="${JIRA_PROJECT}" --jira.issue-type="${JIRA_ISSUE_TYPE}" --jira.open-transition="${JIRA_OPEN_TRANSITION}" --jira.close-transition="${JIRA_CLOSE_TRANSITION}" --jira.open-status="${JIRA_OPEN_STATUS}" --jira.closed-status="${JIRA_CLOSED_STATUS}" --f=. ${CXFLOW_PARAMS} 2> scanid$GITHUB_SHA.txt
